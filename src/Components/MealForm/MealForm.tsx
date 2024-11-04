a053a079b047667316423a95389ffdedb633b454
Dilnoza Tasheva <dilnozatasheva@gmail.com>
2024-11-04 10:42:17 +0100

    Added form and input fields to the MealForm and created functions to send the newly added meal to the server

--- src/Components/MealForm/MealForm.tsx
+++ src/Components/MealForm/MealForm.tsx
@@ -1,10 +1,79 @@
+import { IMealForm } from '../../types';
+import { useState } from 'react';
+import axiosApi from '../../axiosApi.ts';
 
+
+const initialForm = {
+  time: '',
+  description: '',
+  calories: 0,
+};
 
 const MealForm = () => {
+  const [form, setForm] = useState<IMealForm>({...initialForm});
+
+
+  const onChangeField = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {name, value} = e.target;
+    setForm(prevState => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+     await axiosApi.post('meal.json', {...form, calories: Number(form.calories)});
+
+     setForm({...initialForm});
+
+  };
+
   return (
-    <div>
-      Form
-    </div>
+    <form onSubmit={onSubmit}>
+      <h3>Add new meal</h3>
+      <div className="form-group mb-2">
+        <label htmlFor="time">Time of the meal</label>
+        <input
+          type="text"
+          id="time"
+          name="time"
+          className="form-control"
+          value={form.time}
+          onChange={onChangeField}
+        />
+      </div>
+
+      <div className="form-group mb-2">
+        <label htmlFor="description">Description: </label>
+        <input
+          type="text"
+          id="description"
+          name="description"
+          className="form-control"
+          value={form.description}
+          onChange={onChangeField}
+        />
+      </div>
+
+      <div className="form-group mb-2">
+        <label htmlFor="calories">Calories :</label>
+        <input
+          type="number"
+          id="calories"
+          name="calories"
+          min={1}
+          className="form-control"
+          value={form.calories}
+          onChange={onChangeField}
+        />
+      </div>
+
+      <button type="submit" className="btn btn-primary">
+        Add meal
+      </button>
+    </form>
   );
 };
 
---
+++ src/types.d.ts
@@ -0,0 +1,5 @@
+export interface IMealForm {
+  time: string;
+  description: string;
+  calories: number;
+}
